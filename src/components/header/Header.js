0f6e7a362ef4d452aae9af035ff5d7360f096869
Hturoy <69842773+Hturoy@users.noreply.github.com>
2021-09-14 17:06:56 +0200

    Set up file structure, linked fonts in index file. created a globalstyle

---
+++ src/components/header/Header.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+import variables from '../../sass/partials/_layout.scss'
+
+const Wrapper = styled.div ` 
+    background-color: ${variables.$lightgray};
+    widht: 100%;
+    height: 70px;
+
+`
+
+const Header = () => {
+    
+
+
+
+    return (
+        <>
+        <Wrapper/>
+        </>
+    )
+}
+
+export default Header
---
+++ src/global.js
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from "styled-components";
+
+const globalStyle = createGlobalStyle `
+    * {
+        margin: 0;
+        padding: 0;
+    }
+`
