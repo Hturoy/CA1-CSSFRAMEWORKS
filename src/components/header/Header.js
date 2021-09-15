cf6d3698a34c8b2c048bc2743e976a87cd5e645f
Hturoy <69842773+Hturoy@users.noreply.github.com>
2021-09-15 14:29:15 +0200

    Added logo(h1) and navbar content to the header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,41 @@
 import styled from 'styled-components'
 import { Colors } from '../../global'
+import { Fonts } from '../../global'
 
 
 const Wrapper = styled.div ` 
-    background-color: ${Colors.lightGray} ;
     width: 100%;
     height: 70px;
-
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding-left: 100px;
 `
+
+const LogoWrapper = styled.div `
+    color: ${Colors.pink};
+    font-style: italic;
+    display: flex;
+    justify-content: center;
+    width: 168px;
+
+    h1 {
+        font-size: 20px;
+        font-family: ${Fonts.playfair};
+    }
+`
+
+const ContentWrapper = styled.div `
+    display: flex;
+    flex-direction: row;
+    padding-left: 50px;
+    a {
+        color: black;
+        margin: 10px;
+    }
+`
+
+
 
 const Header = () => {
     
@@ -16,7 +44,18 @@ const Header = () => {
 
     return (
         <>
-        <Wrapper/>
+        <Wrapper>
+
+            <LogoWrapper>
+                <h1>The Yay Company</h1>
+            </LogoWrapper>
+
+            <ContentWrapper>
+            <a href="#">Home</a>
+            <a href="#">News</a>
+            <a href="#">Contact</a>
+            </ContentWrapper>
+        </Wrapper>
         </>
     )
 }
--- src/global.js
+++ src/global.js
@@ -2,19 +2,6 @@ import styled from 'styled-components'
 
 import { createGlobalStyle } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle `
-
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        font-size: 16px;
-    }
-
-    
-`
-
-
 
 
 export const Colors = {
@@ -24,12 +11,35 @@ export const Colors = {
     gray: '#767676',
     lightGray: '#C7C7C7',
     darkgray: '#767676',
-    gradient: 'radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(229,229,229,1)'
+    gradient: 'radial-gradient(circle, #FFFFFF 0%, #584949'
 }
 
 export const Fonts = {
     lato: "font-family: 'Lato', sans-serif;",
     playfair: "font-family: 'Playfair Display', serif;"
 }
+
+
+export const GlobalStyle = createGlobalStyle `
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-size: 16px;
+        text-decoration: 0;
+        font-family: ${Fonts.lato};
+    }
+
+    h1 h2 {
+        font-family: ${Fonts.playfair};
+    }
+    
+`
+
+
+
+
+
 
 
